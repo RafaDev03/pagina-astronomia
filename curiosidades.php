2eb2fd705c6cbc39c68618ff520314cfdf0e6a29
Andres Gomez <gomezsalvoandres8@gmail.com>
2025-05-29 00:25:30 -0500

    css 2.0

--- curiosidades.php
+++ curiosidades.php
@@ -4,6 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bulma CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+
+    <!-- Tailwind CSS -->
+    <link rel="stylesheet" href="assets/css/tailwind.css">
     <title>Curiosidades</title>
 </head>
 
