ef2f829de50bf9d78cb9607717e6cc5b1dd4f891
Andres Gomez <gomezsalvoandres8@gmail.com>
2025-05-28 23:27:23 -0500

    aña

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <p>
+        UNIVERSIDAD NACIONAL JOSÉ FAUSTINO SÁNCHEZ CARRIÓN <br>
+        FACULTAD DE INGENIERÍA INDUSTRIAL, SISTEMAS E INFORMÁTICA<br>
+        ESCUELA PROFESIONAL DE INGENIERÍA INFORMÁTICA <br>
+        Asignatura: Desarrollo de Aplicaciones Web<br>
+        Ciclo: IX<br>
+        Docente: Ing. Ronald Flores Flores
+    </p>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<header>
+    <p>906 213 755 // andres08gs@hotmail.com</p>
+    <nav>
+        <ul>
+            <li><a href="perfil.php">Perfil</a></li>
+            <li><a href="estudios.php">Estudios</a></li>
+            <li><a href="experiencia.php">Experiencias</a></li>
+            <li><a href="habilidades.php">Habilidades Tecnicas</a></li>
+            <li><a href="intereses.php">Intereses</a></li>
+            <li><a href="contacto.php">Contáctenos</a></li>
+        </ul>
+    </nav>
+</header>
