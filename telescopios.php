eb293c36d64f17bcb08213949a6bff4efc818ed2
Andres Gomez <gomezsalvoandres8@gmail.com>
2025-05-29 15:25:47 -0500

    los marcianitos

--- telescopios.php
+++ telescopios.php
@@ -1,19 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Telescopios - Astronomía</title>
-    <!-- Bulma CSS -->
-    <link rel="stylesheet" href="../node_modules/bulma/css/bulma.min.css" />
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-
-    <!-- Tailwind CSS -->
-    <link rel="stylesheet" href="assets/css/tailwind.css">
-</head>
+<?php
+include_once 'includes/head.php'
+    ?>
 
 <body>
     <!-- Header -->
